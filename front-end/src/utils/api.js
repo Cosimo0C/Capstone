720f883eef58d964d70a6568a04ca16be486ce2a
Cosimo0C <cosimocrupi01@gmail.com>
2025-10-17 16:07:32 +0200

    fixed wrong name api

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -1,5 +1,5 @@
 // Utility per centralizzare la base URL delle API
-const BASE_URL = import.meta.env.VITE_API_HOST || "carsbuy-data-cosimocrupi01-2cbc.e.aivencloud.com";
+const BASE_URL = import.meta.env.APP_API_URL || "carsbuy-data-cosimocrupi01-2cbc.e.aivencloud.com";
 
 export function apiUrl(path) {
   // assicura che il path inizi con /
